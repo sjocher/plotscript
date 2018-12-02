528646c68cbaf774f214c0017daf10fdc352ee7b
Sean <sjocher@vt.edu>
2018-12-02 15:01:58 -0500

    reverting back to task 2

--- plotscript.cpp
+++ plotscript.cpp
@@ -61,31 +61,18 @@ int eval_from_command(std::string argexp){
 
 // A REPL is a repeated read-eval-print loop
 void repl(std::thread *thread){
-    std::atomic_bool kernalRunning(false);
+    std::atomic_bool kernalRunning(true);
     std::atomic_bool solved(false);
     parseQueue pQ;
     resultQueue rQ;
     parseInterp pI(&pQ, &rQ, &kernalRunning, &solved);
+    *thread = std::thread(pI);
     while(!std::cin.eof()){
         prompt();
         std::string line = readline();
         if(line.empty()) continue;
         if(line.front() == '%') {
-            if(line == "%start") {
-                if(!kernalRunning) {
-                    kernalRunning.store(true);
-                    *thread = std::thread(pI);
-                }
-                continue;
-            } else if (line == "%stop") {
-                kernalRunning.store(false);
-                if(thread->joinable()) {
-                    pQ.push(line);
-                    thread->join();
-                    thread->~thread();
-                }
-                continue;
-            }
+            //nothing
         }
         if(kernalRunning) {
             pQ.push(line);
